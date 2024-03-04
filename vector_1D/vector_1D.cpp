f55059e4f504195ff56208cfa246db5a4218860e
George Pimpleton <george_pimpleton@hotmail.com>
2024-03-04 08:16:48 -0800

    Updated 1D vector example

--- vector_1D/vector_1D.cpp
+++ vector_1D/vector_1D.cpp
@@ -11,6 +11,7 @@ int main( )
    std::vector vec_1d { 1, 2, 3, 4, 5 };
 
    // let's print out the vector, the "old school" way
+   std::cout << "Using a regular for loop....\n";
    for ( size_t i { }; i < vec_1d.size( ); ++i )
    {
       std::cout << vec_1d[i] << ' ' ;
@@ -18,12 +19,23 @@ int main( )
    std::cout << "\n\n";
 
    // let's display the vector using iterators
+   std::cout << "Using iterators in a for loop....\n";
    for ( auto itr { vec_1d.cbegin( ) }; itr != vec_1d.cend( ); ++itr )
    {
       std::cout << *itr << ' ';
    }
    std::cout << "\n\n";
 
+   // let's use a range-based for loop
+   // this is used in the template function
+   std::cout << "Using a range-based for loop....\n";
+   for ( auto const& itr : vec_1d )
+   {
+      std::cout << itr << ' ';
+   }
+   std::cout << "\n\n";
+
    // let's use the template
+   std::cout << "Using a templated function....\n";
    std::cout << vec_1d << '\n';
 }
